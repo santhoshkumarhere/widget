ab867055043ae0156371b5237692e8898eeb0da6
Kumar, Santhosh <santhoshk.kumar@pioneer.com>
2020-04-18 11:37:45 -0500

    initial discount button

--- src/index.js
+++ src/index.js
@@ -1,44 +1,66 @@
 
-var body =  document.getElementsByTagName("BODY")[0];
-body.innerHTML+= '<button id="myBtn" class="widget-button round-button"> Home</button>'
- 
-// Get the modal
-var modal = document.getElementById("myModal");
-var formWrapper = document.getElementById("form-wrapper");
-formWrapper.innerHTML += '<form onsubmit = onSubmit()> <input type="text" id="name"/><button type="submit" id="sign-up" style="border: 0; background: transparent"> <img src="https://www.pinclipart.com/picdir/middle/108-1085256_student-webmail-login-email-icon-png-transparent-small.png" width="90" height="50" alt="submit" /></button></form>';
+const body =  document.getElementsByTagName("BODY")[0];
+const modal = document.getElementById("myModal");
+const companyName = document.getElementById('company-name');
+const discountButtonImageName = document.getElementById('button-name');
+const discountButtonImages = {
+  "bogo": "url('./img/bogo.png')",
+  "gift": "url('./img/gift.png')",
+  "tenPercent": "url('./img/tenPercent.png')",
+  "percentOff": "url('./img/percentOff.png')"
+};
 
-const  companyName = document.getElementById('company-name');
-formWrapper.innerHTML += companyName.value;
-formWrapper.style.backgroundColor = document.getElementById('company-background').value;
+const createDiscountButton = () => {
+  let discountButton = document.createElement("BUTTON");   // Create a <button> element
+  discountButton.className = 'discount-button';
+  discountButton.id ='discount-btn';
+  discountButton.style.backgroundImage = discountButtonImages[discountButtonImageName.value];
 
+  // When the user clicks the button, open the modal 
+  discountButton.onclick = function() {
+    modal.style.display = "block";
+  }
 
-const signUpButton = document.getElementById('sign-up');
-signUpButton.addEventListener('click', onSubmit)
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+  body.appendChild(discountButton);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+const createModalForm = () => {
+  const formWrapper = document.getElementById("form-wrapper");
+  formWrapper.innerHTML += 
+    `<form onsubmit = onSubmit()> 
+        <input type="text" id="name"/>
+        <button type="submit" id="sign-up" style="border: 0; background: transparent">
+          <img src="https://www.pinclipart.com/picdir/middle/108-1085256_student-webmail-login-email-icon-png-transparent-small.png" width="90" height="50" alt="submit" />
+        </button>
+      </form>`;
+  formWrapper.innerHTML += companyName.value;
+  formWrapper.style.backgroundColor = document.getElementById('company-background').value;
+
+  const signUpButton = document.getElementById('sign-up');
+  signUpButton.addEventListener('click', onSubmit) 
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+const closeModalWindow = () => {
+  const span = document.getElementById("close-button");
+  span.onclick = () => {
     modal.style.display = "none";
+  }
+
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
   }
 }
 
+createDiscountButton();
+createModalForm();
+closeModalWindow();
+
+
 function onSubmit(e) {
-e.preventDefault ();
-console.log('prevented')
-//method="post" action ="https://sales.phidev.com/api/catalog/productLine/010/subproduct/NJ87/quantity"
+  e.preventDefault ();
+  console.log('prevented')
+  //method="post" action ="https://sales.phidev.com/api/catalog/productLine/010/subproduct/NJ87/quantity"
 }
+
